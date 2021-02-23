ab9a0e4a8cb970b457cbb9516ffbe1ce2a787e46
柿内暢昌 <nob@studio-kakky.com>
2021-02-23 14:10:19 +0900

    polish index.js

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,7 +1,10 @@
 import Head from 'next/head'
+import Link from 'next/link'
+
 import Layout, { siteTitle } from '../components/layout'
 import utilStyles from '../styles/utils.module.css'
 import { getSortedPostsData } from '../lib/posts'
+import Date from '../components/date'
 
 export default function Home({ allPostsData }) {
   return (
@@ -22,15 +25,19 @@ export default function Home({ allPostsData }) {
         <ul className={utilStyles.list}>
           {allPostsData.map(({ id, date, title }) => (
             <li className={utilStyles.listItem} key={id}>
-              {title}
+              <Link href={`/posts/${id}`}>
+                <a>{title}</a>
+              </Link>
               <br />
-              {id}
-              <br />
-              {date}
+              <small className={utilStyles.lightText}>
+                <Date dateString={date} />
+              </small>
             </li>
           ))}
         </ul>
       </section>
+
+
     </Layout>
   )
 }
