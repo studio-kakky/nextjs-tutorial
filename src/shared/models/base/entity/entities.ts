9b90788c21afda26b0b3e09a411b1c3258ea6038
柿内暢昌 <nob@studio-kakky.com>
2021-02-27 16:39:30 +0900

    fix for lint

--- src/shared/models/base/entity/entities.ts
+++ src/shared/models/base/entity/entities.ts
@@ -1,5 +1,6 @@
 import { BaseEntity } from './entity';
 
+// eslint-disable-next-line @typescript-eslint/no-explicit-any
 export abstract class BaseEntities<T extends BaseEntity<any>> {
   // eslint-disable-next-line no-useless-constructor
   constructor(protected list: T[]) {
