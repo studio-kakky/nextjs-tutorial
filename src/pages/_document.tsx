0e4a64450a952118a6a1687b60e01d34db989c75
柿内暢昌 <nob@studio-kakky.com>
2021-03-19 15:24:08 +0900

    fix for lint

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -1,6 +1,5 @@
 import NextDocument, { Html, Head, Main, NextScript } from 'next/document';
 
-type Props = {};
 const gtmScript = `(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
 new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
 j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
@@ -9,8 +8,8 @@ j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
 const gtmFrame = `<iframe src="https://www.googletagmanager.com/ns.html?id=GTM-PBTQZCR"
 height="0" width="0" style="display:none;visibility:hidden"></iframe>`;
 
-class Document extends NextDocument<Props> {
-  render() {
+class Document extends NextDocument {
+  render(): JSX.Element {
     return (
       <Html>
         <Head>
