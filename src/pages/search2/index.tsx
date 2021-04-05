2a52c8471bfc238fc60217635334a12b85dc2663
柿内暢昌 <nob@studio-kakky.com>
2021-04-05 15:16:12 +0900

    コードの重複を解消

--- src/pages/search2/index.tsx
+++ src/pages/search2/index.tsx
@@ -2,7 +2,6 @@ import { useEffect, useState } from 'react';
 import { useRouter } from 'next/router';
 
 import Layout from '../../layout/layout';
-import { Restaurant } from '../../shared/models/restaurant/restaurant';
 import { getYelpBusiness } from '../../shared/api/business/api';
 import { YelpBusinessApiGetInputModel } from '../../shared/api/business/get-input-model';
 import { adapt } from '../../shared/models/restaurant/adapt';
@@ -11,10 +10,13 @@ import { RestaurantViewModels } from '../../components/restaurants/view-model/vi
 import Restaurants from '../../components/restaurants/restaurants';
 import { executeExperience } from '../../shared/lib/kaizen-platform/execute-experience';
 
-interface Props {
-  location: string;
-  restaurants: Restaurant[];
-}
+const fetchRestaurants = async (location: string, pageNumber: number): Promise<RestaurantViewModels> => {
+  const model = new YelpBusinessApiGetInputModel({ location, pageNumber });
+  const res = await getYelpBusiness(model);
+  const list = adapt(res);
+  const viewModelList = list.map(makeViewModel);
+  return new RestaurantViewModels(viewModelList);
+};
 
 export default function Search(): JSX.Element {
   const [viewModels, setViewModels] = useState(RestaurantViewModels.blank());
@@ -22,22 +24,13 @@ export default function Search(): JSX.Element {
   const [location, setLocation] = useState<string>();
   const router = useRouter();
 
-  const loadMore = async () => {
-    const model = new YelpBusinessApiGetInputModel({ location: location, pageNumber: page + 1 });
-    const res = await getYelpBusiness(model);
-    const list = adapt(res);
-    const viewModelList = list.map(makeViewModel);
-    const nextViewModels = new RestaurantViewModels(viewModelList);
+  const fetch = async () => {
+    const nextViewModels = await fetchRestaurants(location, page);
     setViewModels(viewModels.merge(nextViewModels));
-    setPage(page + 1);
   };
 
-  const initialFetch = async () => {
-    const model = new YelpBusinessApiGetInputModel({ location, pageNumber: 1 });
-    const res = await getYelpBusiness(model);
-    const list = adapt(res);
-    const viewModelList = list.map(makeViewModel);
-    setViewModels(new RestaurantViewModels(viewModelList));
+  const loadMore = async () => {
+    setPage(page + 1);
   };
 
   const onToggleChecked = (id: string) => {
@@ -52,13 +45,19 @@ export default function Search(): JSX.Element {
 
   useEffect(() => {
     if (!!location) {
-      initialFetch().then(() => {
+      fetch().then(() => {
         requestAnimationFrame(() => {
           executeExperience(router);
         });
       });
     }
   }, [location]);
+
+  useEffect(() => {
+    if (!!page && page !== 1) {
+      fetch();
+    }
+  }, [page]);
 
   return (
     <Layout>
